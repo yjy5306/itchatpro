6142d4a1f3c04a53fb7ff1288ab7b317ace303e3
yong168942 <41948363+yong168942@users.noreply.github.com>
2018-09-14 20:28:04 +0900

    ㅎㅎ

--- src/main/java/com/yokipa/itchat/user/bd/service/BRDServuce.java
+++ src/main/java/com/yokipa/itchat/user/bd/service/BRDServuce.java
@@ -2,16 +2,16 @@ package com.yokipa.itchat.user.bd.service;
 
 /**
  * @Class : BRDService
- * @Description : 카테고리검색서비스
+ * @Description : 移댄뀒怨좊━寃��깋�꽌鍮꾩뒪
  * @Type: Service
  * @Modification
  * 
- *  ������      			������              ��������
+ *  占쏙옙占쏙옙占쏙옙      			占쏙옙占쏙옙占쏙옙              占쏙옙占쏙옙占쏙옙占쏙옙
  *  ---------   ---------   -------------------------------
- *  2018.09.06		������		�űԵ��
+ *  2018.09.06		占쏙옙占쏙옙占쏙옙		占신규듸옙占�
  *
  */
 
 public interface BRDServuce {
-	
+	//몰랑
 }
